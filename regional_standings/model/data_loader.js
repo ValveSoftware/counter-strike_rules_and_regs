24ae68512f0f3d3f34acf927c5a596da2f44c098
brianlev-valve <109995526+brianlev-valve@users.noreply.github.com>
2023-02-10 15:22:21 -0800

    Added model code

---
+++ regional_standings/model/data_loader.js
@@ -0,0 +1,213 @@
+"use strict";
+
+const fs = require('fs');
+const Region = require('./util/region');
+const Team = require('./team');
+const remapValueClamped = require('./util/remap_value_clamped');
+
+const __highValueEvents = [6372,6711,6712,6713,6714,6586,6588]; //explicitly include RMR events, Majors
+
+function parsePrizePool( prizePool ) {
+    if ( prizePool === undefined )
+        return 0;
+
+    prizePool = prizePool.replaceAll(',','').replace('$','');
+    if ( /^[0-9]+$/.test(prizePool) )
+        return Number(prizePool);
+
+    return 0; 
+}
+
+function sortMatches( matches, order = 'desc' ) {
+    if ( order === 'asc' )
+        matches.sort( (a, b) => a.matchStartTime - b.matchStartTime );
+    else 
+        matches.sort( (a, b) => b.matchStartTime - a.matchStartTime );
+}
+
+function filterIncompleteMatches( matches ) {
+    return matches.filter( match => ( match.team1Players.length === 5 && match.team2Players.length === 5 ) );
+}
+
+function filterMatchesByTime( matches, startTime, endTime )
+{
+    return matches.filter( match =>
+        ( endTime < 0 || match.matchStartTime <= endTime )
+        && (startTime < 0 || match.matchStartTime >= startTime ) );
+}
+
+class EventTeam {
+    constructor( prizeJson ) {
+        this.placement = prizeJson.placement;
+        this.prize = prizeJson.prize;
+        this.shared = prizeJson.shared;
+    }
+}
+
+class Event {
+    constructor( eventJson ) {
+        this.eventId = eventJson.eventId;
+        this.name = eventJson.eventName;
+        this.prizePool = parsePrizePool( eventJson.prizePool );
+        this.prizeDistributionByTeamId = {};
+        this.lan = eventJson.lan;
+        this.lastMatchTime = -1;
+
+        eventJson.prizeDistribution.forEach( teamJson => {
+            this.prizeDistributionByTeamId[teamJson.teamId] = new EventTeam( teamJson );
+        } );
+    }
+
+    accumulateMatch( match )
+    {
+        this.lastMatchTime = Math.max( this.lastMatchTime, match.matchStartTime );
+    }
+}
+
+function initTeams( matches, events, rankingContext ) {
+    let teams = [];
+
+    function insertTeam( name, players ) {
+        let team = teams.find( team => team.sharesRoster(players) );
+        if( team !== undefined )
+            return team;
+
+        let rosterId = teams.length;
+        team = new Team( rosterId, name, players );
+        teams.push( team );
+        return team;
+    }
+
+    matches.forEach( match => {
+        match.team1 = insertTeam( match.team1Name, match.team1Players );
+        match.team2 = insertTeam( match.team2Name, match.team2Players );
+
+        match.team1.accumulateMatch( match );
+        match.team2.accumulateMatch( match );
+
+        // If we have event data for this match, give the teams a shot at it.
+        // This is the only point where the teamId for the team matches the data
+        // for that event.
+        if( match.eventId !== undefined )
+        {
+            match.team1.recordEventParticipation( events[match.eventId], match.team1Id );
+            match.team2.recordEventParticipation( events[match.eventId], match.team2Id );
+        }
+    } );
+
+    // Calculate seeding data for each team based on professional performance
+    // (quantity/quality of professional teams defeated and prizes won)
+    Team.initializeSeedingModifiers( teams, rankingContext );
+
+    return teams;
+}
+
+function calculateMatchInformationContent( match, rankingContext, events ){
+    let informationContent = 1.0;
+    informationContent *= rankingContext.getTimestampModifier( match.matchStartTime );
+
+    return informationContent;
+}
+
+function findTimeWindow( matches, filterEnd, dataWindow )
+{
+    let endTime = filterEnd;
+    if ( endTime < 0 )
+        endTime = Math.max( ...matches.map( match => match.matchStartTime ) );
+
+    let startTime = endTime - dataWindow;
+
+    return [startTime, endTime];
+}
+
+class DataLoader
+{
+    constructor( rankingContext ) {
+        this.matches = [];
+        this.events = {};
+        this.teams = {};
+        this.filterEndTime = -1;
+        this.filterWindow = 6*30*24*3600;
+        this.rankingContext = rankingContext;
+    }
+
+    setTimeFilter( endTime = -1, dataWindow = 6*30*24*3600 ) {
+        this.filterEndTime = endTime;
+        this.filterWindow = dataWindow;
+        return this;
+    }
+
+    clearTimeFilter() {
+        this.filterEndTime = -1;
+        this.filterWindow = -1;
+        return this;
+    }
+
+    getContext() {
+        return this.context;
+    }
+
+    // How much extra weight do we put on RMR/major events
+    setHveMod( value ) { this.rankingContext.setHveMod( value ); }
+
+    // When looking at data about a team's performance, how many outliers do we ignore?
+    // e.g. the team with the most prize winnings is not treated differently than the team
+    // with the nth most prize winnings.
+    setNthHighest( nth ) { this.rankingContext.setOutlierCount( nth ); }
+
+    loadData()
+    {
+        const data = fs.readFileSync( '../data/matchdata.json' );
+        const dataJson = JSON.parse( data );
+
+        // initialize match list
+        let matches = dataJson.matches;
+
+        // Filter matches to only the data we are interested in.
+        matches = filterIncompleteMatches( matches );
+        const [startTime,endTime] = findTimeWindow( matches, this.filterEndTime, this.filterWindow );
+        let graceperiod = 30 * 24 * 3600; // 1 month
+        this.rankingContext.setTimeWindow( startTime, endTime - graceperiod );
+        matches = filterMatchesByTime( matches, startTime, endTime );
+        
+        // initialize event list
+        let events = {};
+        dataJson.events.forEach( eventJson => events[eventJson.eventId] = new Event( eventJson ) );
+
+        // Let each event know what matches were part of it
+        matches.forEach( match => {
+            let event = events[match.eventId];
+            if( event !== undefined )
+                event.accumulateMatch( match );
+        } );
+
+        // Estimate the information content of each match (for example, recent matches may be considered
+        // to have more accurate data about the current skill of players than old ones)
+        matches.forEach( match => {
+            match.informationContent = calculateMatchInformationContent( match, this.rankingContext, events );
+        } );
+
+        // Now that we have events ready, we can initialize teams.  We are going to translate from the
+        // input data `teamId` to a more granular `rosterId` that is based on the players that play in
+        // the teams (similarly to Major qualification rules).  Rosters that share enough players with
+        // a more recent match are considered the "same" team for the purposes of ranking.
+
+        // When initializing the teams, sort matches by reverse start time so we always see the
+        // most recent match for a particular roster as the 'base' roster for that team.
+        sortMatches( matches, 'desc' );
+
+        let teams = initTeams( matches, events, this.rankingContext );
+
+        // For processing the games and calculating ratings, we will go in forward order in time.  This
+        // also has the effect of making sure that recent data is considered the most strongly, and also
+        // with as much context as possible given past results.
+        sortMatches( matches, 'asc' );
+
+        this.matches = matches;
+        this.teams = teams;
+        this.events = events;
+    }
+}
+
+module.exports = DataLoader;
+
---
+++ regional_standings/model/team.js
@@ -0,0 +1,191 @@
+"use strict";
+
+const nthHighest = require('./util/nth_highest');
+const Region = require('./util/region');
+const TEAM_OVERLAP_TO_SHARE_ROSTER = 3;
+
+class TeamEvent {
+    constructor( event, teamId ) {
+        this.event = event;
+        this.teamId = teamId;
+
+        let prizeEntry = event.prizeDistributionByTeamId[teamId];
+        this.winnings = ( prizeEntry === undefined ) ? 0 : prizeEntry.prize;
+    }
+
+    getTeamWinnings() {
+        return this.winnings;
+    }
+}
+
+class TeamMatch {
+    constructor( team, match ) {
+        if( match.team1 !== team && match.team2 !== team )
+        {
+            throw new Error( "adding a match to a team that didn't participate in it" );
+        }
+
+        this.match      = match;
+        this.team       = team;
+        this.teamNumber = ( match.team1 === team ) ? 1 : 2;
+        this.isWinner   = match.winningTeam === this.teamNumber;
+        this.opponent   = ( this.teamNumber === 1 ) ? match.team2 : match.team1;
+    }
+}
+
+function getPluralityRegion( players ) {
+    let teamCountries = players.map( el => el.countryIso );
+    let regionAssignment = [0, 0, 0]; //EU, AMER, ROW
+
+    teamCountries.forEach( el => { 
+        regionAssignment[Region.getCountryRegion(el)]+=1 
+    });
+
+    let region = regionAssignment.findIndex( el => el === Math.max(...regionAssignment) );
+    return region;
+}
+
+
+class Team {
+    static TeamMatch = TeamMatch;
+    static TeamEvent = TeamEvent;
+
+    constructor( rosterId, name, players ) {
+        this.rosterId = rosterId;
+        this.name = name;
+        this.players = players;
+        this.teamMatches = [];
+        this.wonMatches = [];
+        this.matchesPlayed = 0;
+        this.eventMap = new Map();
+        this.lastPlayed = 0;
+        this.modifiers = {};
+        this.region = getPluralityRegion( this.players );
+    }
+
+    // A past team is considered as the same entity as a more recent one,
+    // if it shares enough players.
+    sharesRoster( players )
+    {
+        let overlap = 0;
+        players.forEach( pNew => {
+            this.players.forEach( pExisting => {
+                if( pNew.playerId === pExisting.playerId ) {
+                    overlap += 1;
+                }
+            } );
+        } );
+
+        return ( overlap >= TEAM_OVERLAP_TO_SHARE_ROSTER );
+    }
+
+    // We only link the team ids from the source data to our teams in the context
+    // of an event, because the same team might replace its players and thus would
+    // not be considered the same roster -- we don't want to credit the new roster
+    // with the old teams winnings, and we do want to credit some players with their
+    // past winnings after they have changed teams.
+    recordEventParticipation( event, teamId )
+    {
+        if( event === undefined )
+            return;
+
+        if( this.eventMap.has( event.eventId ) )
+            return;
+
+        this.eventMap.set( event.eventId, new TeamEvent( event, teamId ) );
+    }
+
+    accumulateMatch( match )
+    {
+        const teamMatch = new TeamMatch( this, match )
+
+        this.teamMatches.push( teamMatch );
+        if( teamMatch.isWinner ) {
+            this.wonMatches.push( teamMatch );
+        }
+    }
+
+    static initializeSeedingModifiers( teams, context )
+    {
+        // no work to do
+        if( teams.length === 0 )
+            return;
+
+        // Phase 1: Do calculations we can do directly from this team's data -- we don't rely on
+        // any other team info to figure this data out.
+        teams.forEach( team => {
+            team.matchesPlayed = team.teamMatches.length;
+            team.lastPlayed = Math.max( ...team.teamMatches.map( teamMatch => teamMatch.match.matchStartTime ) );
+            team.distinctTeamsDefeated = 0;
+            team.scaledPrizePool = 0;
+
+            // Calculate the most recent match against each opponent
+            // We are going to use this to discount the prestige factor if you haven't defeated a team recently.
+            // (It's still better to have defeated them than not, for seeding purposes, just not as good as
+            // if they were defeated recently)
+            let opponentMap = new Map();
+            team.wonMatches.forEach( wonMatch => {
+                let opp = wonMatch.opponent;
+                let matchTime = wonMatch.match.matchStartTime;
+                let prevBestMatchTime = opponentMap.get( opp );
+                if( prevBestMatchTime === undefined || prevBestMatchTime < matchTime )
+                {
+                    opponentMap.set( opp, matchTime );
+                }
+            });
+
+            opponentMap.forEach( ( lastWinTime, opp ) => {
+                team.distinctTeamsDefeated += context.getTimestampModifier( lastWinTime );
+            } );
+    
+            // Also calculate winnings
+            team.eventMap.forEach( teamEvent => {
+                team.scaledPrizePool += teamEvent.getTeamWinnings() * context.getTimestampModifier( teamEvent.event.lastMatchTime );
+            } );
+        } );
+
+        // Phase 2 relies on the data from *all* teams in phase 1 being calculated.
+        // we want to know relative data -- such as whether this team's winnings are representative
+        // of the top teams in the world, or if a team has beaten a typical number of opponents.
+        let referencePrizePool     = nthHighest( teams.map( t => t.scaledPrizePool ), context.getPrizePoolNth() );
+        let referenceOpponentCount = nthHighest( teams.map( t => t.distinctTeamsDefeated ), context.getDistinctOpponentNth() );
+
+        teams.forEach( team => {
+            team.winnings = Math.min( team.scaledPrizePool / referencePrizePool, 1 );
+            team.teamsDefeated = Math.min( team.distinctTeamsDefeated / referenceOpponentCount, 1 );
+        } );
+
+        // Phase 3 looks at each team's opponents and rates each team highly if it can regularly win against other prestigous teams.
+        teams.forEach( team => {
+            let sumScaledPrizeContribution = 0;
+            let sumScaledOpponentContribution = 0;
+            let sumScaledDenominator = 1;
+    
+            team.wonMatches.forEach( teamMatch => {
+                let matchModifier = context.getTimestampModifier( teamMatch.match.matchStartTime );
+
+                sumScaledPrizeContribution += teamMatch.opponent.winnings * matchModifier;
+                sumScaledOpponentContribution += teamMatch.opponent.teamsDefeated * matchModifier;
+                sumScaledDenominator += matchModifier;
+            } );
+    
+            sumScaledDenominator = Math.max(10,sumScaledDenominator); // in case you don't have much data or it's all very old
+        
+            team.opponentWinnings = sumScaledPrizeContribution / sumScaledDenominator;
+            team.opponentVictories = sumScaledOpponentContribution / sumScaledDenominator;
+        } );
+
+        // Finally, build modifiers from calculated values
+        function curveFunction( x ) { return Math.pow( 1 / ( 1 + Math.abs(Math.log10(x)) ), 1 ); }
+        function powerFunction( x ) { return Math.pow( x, 1 ) };
+        
+        teams.forEach( team => {
+            team.modifiers.bounty       = curveFunction( team.opponentWinnings );
+            team.modifiers.selfBounty   = curveFunction( team.winnings );
+            team.modifiers.opponent     = powerFunction( team.opponentVictories );
+            team.modifiers.selfOpponent = powerFunction( team.teamsDefeated );
+        } );
+    }
+}
+
+module.exports = Team;
